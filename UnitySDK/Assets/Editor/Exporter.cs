8c2e269de39f11b54b7afe0410cc8fcc4673e410
Atralupus <me@atralupus.com>
2022-06-17 16:34:05 +0900

    Add exporter

---
+++ UnitySDK/Assets/Editor/Exporter.cs
@@ -0,0 +1,25 @@
+
+using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public static class Exporter
+{
+    public static void Export()
+    {
+        string pluginsPath = "Assets/Plugins/";
+        var exportPath = "../Build/SDK.unitypackage";
+
+        string[] pluginsFiles = (string[])Directory.GetFiles(pluginsPath, "*.*", SearchOption.AllDirectories);
+        string[] files = new string[pluginsFiles.Length];
+
+        pluginsFiles.CopyTo(files, 0);
+
+        AssetDatabase.ExportPackage(
+            files,
+            exportPath,
+            ExportPackageOptions.Default);
+    }
+}
