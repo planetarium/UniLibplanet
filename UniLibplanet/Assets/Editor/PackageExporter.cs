e3d3191cad696535be48293a9ba88ab1886756c1
Atralupus <me@atralupus.com>
2022-06-27 18:01:02 +0900

    Check Libplanet.Unity.dll exists

--- UniLibplanet/Assets/Editor/PackageExporter.cs
+++ UniLibplanet/Assets/Editor/PackageExporter.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 
 public static class PackageExporter
 {
-    private const string OutPath = "out/";
-    private const string PluginsPath = "Assets/Plugins/";
-    private const string ScriptsPath = "Assets/Scripts/";
+    private const string OutPath = "out";
+    private const string PluginsPath = "Assets/Plugins";
+    private const string ScriptsPath = "Assets/Scripts";
     private const string SDKName = "UniLibplanet.unitypackage";
 
     [MenuItem("Tools/Build SDK")]
     public static void Export()
     {
-        Debug.Log("Build Start");
-
         DirectoryInfo projectRoot = Directory.GetParent(Directory.GetCurrentDirectory());
         var outputDirectory = new DirectoryInfo(Path.Combine(projectRoot.FullName, OutPath));
         string exportPath = Path.Combine(outputDirectory.FullName, SDKName);
         
         outputDirectory.Create();
+
+        if (!File.Exists(Path.Combine(PluginsPath, "Libplanet.Unity.dll")))
+        {
+            Debug.Log("First, please build Libplanet.Unity.");
+            return;
+        }
 
         var exportedPackageAssetList = new List<string>();
         exportedPackageAssetList.Add(PluginsPath);
