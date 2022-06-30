de3e4fd7875d25f7c2f7e9c1bf06d64aa5ce8fc8
Hong Minhee <hong.minhee@planetariumhq.com>
2022-06-30 19:43:54 +0900

    Let build scripts run regardless of CWD

--- scripts/build-unity.ps1
+++ scripts/build-unity.ps1
@@ -2,6 +2,8 @@ $DOTENV_PATH = ".\.env.xml"
 $UNITY_DIR = (Select-Xml -Path $DOTENV_PATH -XPath "//UNITY_DIR[1]" | Select-Object -ExpandProperty Node).InnerText
 $UNITY_PATH = Join-Path -Path $UNITY_DIR -ChildPath "Unity.exe"
 
+Push-Location (Split-Path -Parent $PSScriptRoot)
 Write-Host "Unity build start..."
 & $UNITY_PATH -batchmode -quit -logFile debug.txt -nographics -projectPath ".\UniLibplanet" -executeMethod PackageExporter.Export
 Write-Host "Unity build finished"
+Pop-Location
