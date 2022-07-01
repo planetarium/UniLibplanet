8d862fcbf519810f222205785ff5213870e531c4
Hong Minhee <hong.minhee@planetariumhq.com>
2022-07-01 14:26:04 +0900

    Agent is no more MonoSingleton<T>
    Close https://github.com/planetarium/UniLibplanet/issues/32

--- Libplanet.Unity/Agent.cs
+++ Libplanet.Unity/Agent.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using Bencodex.Types;
@@ -23,18 +24,32 @@ namespace Libplanet.Unity
     /// Agent runs <see cref="Miner"/>, <see cref="SwarmRunner"/> and Action Controller
     /// You can use <c>RunOnMainThread</c>, <c>MakeTransaction</c> to manage actions.
     /// </summary>
-    public class Agent : MonoSingleton<Agent>
+    [SuppressMessage(
+        "SonarLint.Bug",
+        "S3453",
+        Justification = "It's only instantiated by GameObject.AddComponent<T>() method.")]
+    public class Agent : MonoBehaviour
     {
         private readonly ConcurrentQueue<System.Action> _actions =
             new ConcurrentQueue<System.Action>();
 
         private Miner _miner;
+        private Coroutine _minerCo;
 
         private Swarm<PolymorphicAction<ActionBase>> _swarm;
 
         private SwarmRunner _swarmRunner;
+        private Coroutine _swarmRunnerCo;
 
         private BlockChain<PolymorphicAction<ActionBase>> _blockChain;
+        private Coroutine _processActionsCo;
+
+        /// <summary>
+        /// Use <see cref="AddComponentTo"/> instead.
+        /// </summary>
+        private Agent()
+        {
+        }
 
         /// <summary>
         /// Address of the <see cref="PrivateKey"/>.
@@ -44,17 +59,26 @@ namespace Libplanet.Unity
         private PrivateKey PrivateKey { get; set; }
 
         /// <summary>
-        /// Initialize Agent.
-        /// Because it is <see cref="MonoSingleton{T}"/>, use Initialize instead of constructor.
+        /// Instantiates <see cref="Agent"/> and add it to the specified
+        /// <paramref name="gameObject"/> as its component.
         /// </summary>
-        /// <param name="renderers">Listener to check status.</param>
-        public static void Initialize(
-            IEnumerable<IRenderer<PolymorphicAction<ActionBase>>> renderers)
+        /// <param name="gameObject">A <see cref="GameObject"/> to add a new <see cref="Agent"/> as
+        /// its component.</param>
+        /// <param name="renderers">Renderers that listen to chain state updates.</param>
+        /// <returns>A new <see cref="Agent"/> instance.</returns>
+        public static Agent AddComponentTo(
+            GameObject gameObject,
+            IEnumerable<IRenderer<PolymorphicAction<ActionBase>>> renderers
+        )
         {
-            if (Instance is { } instance)
-            {
-                instance.InitAgent(renderers);
-            }
+            Agent self = gameObject.AddComponent<Agent>();
+            PrivateKey privateKey = Utils.LoadPrivateKey(Paths.PrivateKeyPath);
+            self.PrivateKey = privateKey;
+            self.Address = privateKey.ToAddress();
+
+            self.ConfigureNode(renderers);
+            self._miner = new Miner(self._swarm, self.PrivateKey);
+            return self;
         }
 
         /// <summary>
@@ -111,28 +135,27 @@ namespace Libplanet.Unity
         /// <summary>
         /// Dispose <see cref="Swarm{T}"/> and <see cref="NetMQConfig"/> clean up.
         /// </summary>
-        protected override void OnDestroy()
+        protected void OnDestroy()
         {
             NetMQConfig.Cleanup(false);
-
-            base.OnDestroy();
             _swarm?.Dispose();
         }
 
-        private void InitAgent(
-            IEnumerable<IRenderer<PolymorphicAction<ActionBase>>> renderers)
+        private void Start()
         {
-            ConfigureKeys();
-            ConfigureNode(renderers);
-            ConfigureMiner();
-
-            StartCoroutines();
+            _swarmRunnerCo = StartCoroutine(_swarmRunner.CoSwarmRunner());
+            _minerCo = StartCoroutine(_miner.CoStart());
+            _processActionsCo = StartCoroutine(CoProcessActions());
         }
 
-        private void ConfigureKeys()
+        private void OnApplicationQuit()
         {
-            PrivateKey = Utils.LoadPrivateKey(Paths.PrivateKeyPath);
-            Address = PrivateKey.PublicKey.ToAddress();
+            StopCoroutine(_processActionsCo);
+            _processActionsCo = null;
+            StopCoroutine(_minerCo);
+            _minerCo = null;
+            StopCoroutine(_swarmRunnerCo);
+            _swarmRunnerCo = null;
         }
 
         private void ConfigureNode(
@@ -157,20 +180,6 @@ namespace Libplanet.Unity
             _swarmRunner = new SwarmRunner(_swarm, PrivateKey);
 
             _blockChain = _swarm.BlockChain;
-        }
-
-        private void ConfigureMiner()
-        {
-            _miner = new Miner(
-                _swarm,
-                PrivateKey);
-        }
-
-        private void StartCoroutines()
-        {
-            StartCoroutine(_swarmRunner.CoSwarmRunner());
-            StartCoroutine(_miner.CoStart());
-            StartCoroutine(CoProcessActions());
         }
 
         private IEnumerator CoProcessActions()
