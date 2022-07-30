d520ed31330b9ec59bf30b7bdba988b7398180b5
Kim Hyeonwoo <1reds@naver.com>
2022-07-30 17:18:52 +0900

    Add Serillog Functions

--- Libplanet.Unity/Utils.cs
+++ Libplanet.Unity/Utils.cs
@@ -4,7 +4,8 @@ using Libplanet.Blocks;
 using Libplanet.Crypto;
 using Libplanet.Node;
 using Libplanet.Store;
-using Libplanet.Unity;
+using Serilog;
+using Serilog.Events;
 
 namespace Libplanet.Unity
 {
@@ -119,6 +120,44 @@ namespace Libplanet.Unity
         public static (IStore Store, IStateStore StateStore) LoadStore(string path)
         {
             return NodeUtils<PolymorphicAction<ActionBase>>.LoadStore(path);
+        }
+
+        /// <summary>
+        /// Creates a new log file which is named to <paramref name="fileName"/>.
+        /// </summary>
+        /// <param name="fileName">The path to save a file.</param>
+        /// <param name="logLevel">Set Log Event Level which is want to write log sentences.</param>
+        /// <param name="logMessage">The message to write what we want to log.</param>
+        public static void WriteSerilog(string fileName, LogEventLevel logLevel, string logMessage)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(fileName)
+                .CreateLogger();
+
+            switch (logLevel)
+            {
+            case LogEventLevel.Verbose:
+                Log.Verbose(logMessage);
+                break;
+            case LogEventLevel.Debug:
+                Log.Debug(logMessage);
+                break;
+            case LogEventLevel.Information:
+                Log.Information(logMessage);
+                break;
+            case LogEventLevel.Warning:
+                Log.Warning(logMessage);
+                break;
+            case LogEventLevel.Error:
+                Log.Error(logMessage);
+                break;
+            case LogEventLevel.Fatal:
+                Log.Fatal(logMessage);
+                break;
+            }
+
+            Log.CloseAndFlush();
         }
     }
 }
